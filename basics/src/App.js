f2fcafbb4a0a9fff7bb81c00ddb5ec87fb5c81ea
Balasri-Pitta <balasripitta@gmail.com>
2025-01-29 18:07:45 +0530

    props concept

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,7 +1,14 @@
-
+import Home from './Home'
+import Contact from './Contact'
+import About from './About'
 function App() {
   return (
-   <h1>HELLO</h1>
+    <div>
+   <h1>HELLO BALA</h1>
+  <Home />
+  <Contact />
+  <About />
+   </div>
          
   );
 }
